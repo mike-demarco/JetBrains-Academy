386d7f45df922a548b46a95109f1d30575e96072
Michael DeMarco <michael.demarco@uconn.edu>
2022-07-11 09:31:01 -0400

    JetBrains Academy 07/11/2022 TheMinimumValueOfAnArray

---
+++ src/TheMinimumValueOfAnArray.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+class TheMinimumValueOfAnArray {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int[] input = new int[scanner.nextInt()];
+
+        for (int i = 0; i < input.length; i++) {
+            input[i] = scanner.nextInt();
+        }
+
+        int minimum = IntStream.of(input).min().orElse(Integer.MAX_VALUE);
+        System.out.print(minimum);
+    }
+}
