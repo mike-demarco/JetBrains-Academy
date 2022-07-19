6b4eeccf2392a61ec71e757b322ccc1127904308
Michael DeMarco <michael.demarco@uconn.edu>
2022-07-19 10:12:16 -0400

    JetBrains Academy 07/19/2022 FixingArithmeticException Calculator createFullName RaiseToThePower ReplacingAWithB SimpleCalculator SimpleCalculatorMain SumArrayElementsGreaterThanAValue TheSequence printLength

---
+++ src/Calculator.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // start coding here
+        String input = scanner.next();
+
+        switch (input) {
+            case "one": {
+                System.out.print(1);
+                break;
+            }
+            case "two": {
+                System.out.print(2);
+                break;
+            }
+            case "three": {
+                System.out.print(3);
+                break;
+            }
+            case "four": {
+                System.out.print(4);
+                break;
+            }
+            case "five": {
+                System.out.print(5);
+                break;
+            }
+            case "six": {
+                System.out.print(6);
+                break;
+            }
+            case "seven": {
+                System.out.print(7);
+                break;
+            }
+            case "eight": {
+                System.out.print(8);
+                break;
+            }
+            case "nine": {
+                System.out.print(9);
+                break;
+            }
+            default: {
+                System.out.print("Out of range!");
+                break;
+            }
+        }
+    }
+}
---
+++ src/SimpleCalculator.java
@@ -0,0 +1,41 @@
+class SimpleCalculator {
+
+    // Implement your methods here
+    public static void subtractTwoNumbers(long a, long b) {
+        System.out.print(a - b);
+    }
+
+
+    public static void sumTwoNumbers(long a, long b) {
+        System.out.print(a + b);
+    }
+
+
+    public static void divideTwoNumbers(long a, long b) {
+        if (b == 0) {
+            System.out.print("Division by 0!");
+        } else {
+            System.out.print(a / b);
+        }
+    }
+
+
+    public static void multiplyTwoNumbers(long a, long b) {
+        System.out.print(a * b);
+    }
+
+    // Implemented method
+    public static void power(long n, long p) {
+        long number = n;
+        long power = p;
+        long result = 1;
+        while (power > 0) {
+            if (power % 2 != 0) {
+                result *= number;
+            }
+            power /= 2;
+            number *= number;
+        }
+        System.out.println(result);
+    }
+}
